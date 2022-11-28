781b53f456e038c89a363cf4c83b119a58111dbd
Junior Plays <50379079+Junior1Plays@users.noreply.github.com>
2022-11-28 16:50:42 -0300

    Create dahack.js

---
+++ dahack.js
@@ -0,0 +1,20 @@
+function DAHelp() {
+  console.log("----------------------------------------------------")
+  console.log("DAAnswer() : Show current answer value")
+  console.log("DACAnswer() : Change current answer value")
+  console.log("Made by JuniorSchueller")
+  console.log("----------------------------------------------------")
+}
+function DAAnswer() {
+    console.log("----------------------------------------------------")
+    console.log("Answer: " + answer)
+    console.log("Made by JuniorSchueller")
+    console.log("----------------------------------------------------")
+}
+function DAAnswer(newvalue) {
+    answer = newvalue
+    console.log("----------------------------------------------------")
+    console.log("Answer set to: " + newvalue)
+    console.log("Made by JuniorSchueller")
+    console.log("----------------------------------------------------")
+}
